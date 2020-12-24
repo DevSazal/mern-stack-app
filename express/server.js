fbbdb65dae77e3be9ead9530a8b26d918a860e5e
DevSazal <se.sazal836@gmail.com>
2020-12-24 14:03:54 +0600

    build expressJS

---
+++ express/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// initialize express & port
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
